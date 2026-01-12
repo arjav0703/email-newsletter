631e995adac05a5e3140fc75b0f496bc9e27935f
Arjav Jain <arjavjain0703@gmail.com>
2026-01-12 21:29:25 +0530

    test saved user details

--- tests/test.rs
+++ tests/test.rs
@@ -1,6 +1,6 @@
 #[tokio::test]
 async fn test_health_status() {
-    let address = spawn_app().await;
+    let address = spawn_app().await.address;
 
     let response = reqwest::get(format!("http://{address}/status"))
         .await
@@ -9,29 +9,39 @@ async fn test_health_status() {
     assert_eq!(response.content_length(), Some(0));
 }
 
-async fn spawn_app() -> String {
+struct TestApp {
+    address: String,
+    db_pool: PgPool,
+}
+
+async fn spawn_app() -> TestApp {
     let port = rand::random_range(2000..9000);
     let address = format!("127.0.0.1:{}", port);
     let config = Settings::from_yaml()
         .await
         .expect("Failed to read configuration.");
-    let connection = config
+    let pool = config
         .database_settings
         .connect()
         .await
         .expect("Failed to connect to DB.");
 
-    let server = email_newsletter::run(&address, connection).expect("Failed to bind address");
+    let server = email_newsletter::run(&address, pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
-    address
+    let app = TestApp {
+        address,
+        db_pool: pool,
+    };
+    app
 }
 
 use email_newsletter::config::Settings;
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 #[tokio::test]
 async fn test_subscribe() {
-    let address = spawn_app().await;
+    let app = spawn_app().await;
+    let address = &app.address;
     let test_data = vec![
         ("name=le%20guin&email=ursula_le_guin%40gmail.com", 200),
         ("name=le%20guin", 400),
@@ -42,11 +52,6 @@ async fn test_subscribe() {
     let config = Settings::from_yaml()
         .await
         .expect("Failed to read configuration.");
-    let connection = config
-        .database_settings
-        .connect()
-        .await
-        .expect("Failed to connect to DB.");
 
     let client = reqwest::Client::new();
 
@@ -67,4 +72,11 @@ async fn test_subscribe() {
         );
         println!("Test passed for body: {}", body);
     }
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }

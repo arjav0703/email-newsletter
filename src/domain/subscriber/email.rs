c3f0e5b6c45c1f4173139d526510d927b353e9e7
Arjav Jain <arjavjain0703@gmail.com>
2026-01-22 22:46:26 +0530

    add tests for email validation

--- src/domain/subscriber/email.rs
+++ src/domain/subscriber/email.rs
@@ -1,9 +1,11 @@
+use validator::validate_email;
+
 #[derive(Debug)]
 pub struct SubscriberEmail(String);
 
 impl SubscriberEmail {
     pub fn parse(email: String) -> Result<Self, String> {
-        if email.contains('@') {
+        if validate_email(&email) {
             Ok(SubscriberEmail(email))
         } else {
             Err("Subscriber email must contain '@'".into())
@@ -14,5 +16,29 @@ impl SubscriberEmail {
 impl AsRef<str> for SubscriberEmail {
     fn as_ref(&self) -> &str {
         &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberEmail;
+    use claims::assert_err;
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "ursuladomain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@domain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
     }
 }

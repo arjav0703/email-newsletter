5fc91b98262fb92f145a44a6fab6075a27fc6dc7
Arjav Jain <arjavjain0703@gmail.com>
2026-02-05 12:12:28 +0530

    confirm subscription status

--- src/routes/confirm_subscription.rs
+++ src/routes/confirm_subscription.rs
@@ -1,10 +1,9 @@
-// use crate::{domain::Subscriber, email_client::EmailClient};
+use crate::email_client::EmailClient;
 use actix_web::{HttpResponse, web};
-// use anyhow::Result;
-// use chrono::Utc;
-// use sqlx::PgPool;
-// use tracing::{error, info, instrument::Instrument};
-// use uuid::Uuid;
+use anyhow::Result;
+use sqlx::PgPool;
+use tracing::{error, info};
+use uuid::Uuid;
 
 #[derive(serde::Deserialize, Debug)]
 pub struct QueryData {
@@ -13,15 +12,78 @@ pub struct QueryData {
 
 #[tracing::instrument(
     name = "Confirming pending subscription (/confirm)",
-    skip(formdata),
-    // fields(
-    //     subscriber_email = %form.email,
-    //     subscriber_name = %form.name
-    // )
+    skip(query_parameters)
 )]
-pub async fn confirm_subsciption(formdata: web::Query<QueryData>) -> HttpResponse {
-    if formdata.subscription_token.is_empty() {
+pub async fn confirm_subsciption(
+    query_parameters: web::Query<QueryData>,
+    connection: web::Data<PgPool>,
+    email_client: web::Data<EmailClient>,
+) -> HttpResponse {
+    let token = query_parameters.subscription_token.clone();
+
+    if token.is_empty() {
         return HttpResponse::BadRequest().finish();
     }
-    HttpResponse::Ok().finish()
+
+    let id = match get_subscriber_id_from_token(&connection, &token).await {
+        Ok(id) => id,
+        Err(_) => {
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
+
+    match id {
+        Some(id) => {
+            if set_id_confirmed(&connection, id).await.is_err() {
+                return HttpResponse::InternalServerError().finish();
+            } else {
+                return HttpResponse::Ok().finish();
+            }
+        }
+        None => {
+            error!("No subscriber found for the provided token");
+            HttpResponse::Unauthorized().finish()
+        }
+    }
+}
+
+#[tracing::instrument(
+    name = "Fetching subscription token details from database",
+    skip(connection, subscription_token)
+)]
+async fn get_subscriber_id_from_token(
+    connection: &PgPool,
+    subscription_token: &str,
+) -> Result<Option<Uuid>> {
+    let result = sqlx::query!(
+        "SELECT subscriber_id FROM subscription_tokens \
+      WHERE subscription_token = $1",
+        subscription_token,
+    )
+    .fetch_optional(connection)
+    .await
+    .map_err(|e| {
+        tracing::error!("Query Failed: {:?}", e);
+        e
+    })?;
+
+    Ok(result.map(|r| r.subscriber_id))
+}
+
+#[tracing::instrument(name = "Marking subscriber as confirmed", skip(connection, id))]
+async fn set_id_confirmed(connection: &PgPool, id: Uuid) -> Result<()> {
+    sqlx::query!(
+        "UPDATE subscriptions \
+      SET status = 'confirmed' \
+      WHERE id = $1",
+        id,
+    )
+    .execute(connection)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to update subscription status: {:?}", e);
+        e
+    })?;
+
+    Ok(())
 }

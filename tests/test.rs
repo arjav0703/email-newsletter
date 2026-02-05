c01a08290d97d6649ef7d4a30d5767170fd8df4b
Arjav Jain <arjavjain0703@gmail.com>
2026-02-05 15:25:38 +0530

    add confirmation test

--- tests/test.rs
+++ tests/test.rs
@@ -77,13 +77,14 @@ async fn test_subscribe() {
         );
         println!("Test passed for body: {}", body);
     }
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
 
     assert_eq!(saved.email, "arjavjain0703@gmail.com");
     assert_eq!(saved.name, "arjav");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 pub async fn configure_database(config: &Settings) -> PgPool {
@@ -125,11 +126,45 @@ async fn confirmations_without_token_are_rejected_with_a_400() {
     let app = spawn_app().await;
 
     let url = format!("http://{}/subscriptions/confirm", app.address);
-    dbg!(&url);
 
-    let response = reqwest::get(&format!("http://{}/subscriptions/confirm", app.address))
-        .await
-        .unwrap();
+    let response = reqwest::get(url).await.unwrap();
 
     assert_eq!(response.status().as_u16(), 400);
+}
+
+#[tokio::test]
+async fn confirmation_link_returns_a_200_if_token_is_valid() {
+    let app = spawn_app().await;
+
+    let client = reqwest::Client::new();
+    let body = "name=arjav&email=arjavjain0703%40gmail.com";
+
+    client
+        .post(format!("http://{}/subscribe", app.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    let saved = sqlx::query!("SELECT subscription_token FROM subscription_tokens",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    let url = format!(
+        "http://{}/subscriptions/confirm?subscription_token={}",
+        app.address, saved.subscription_token
+    );
+
+    let response = reqwest::get(url).await.unwrap();
+
+    assert_eq!(response.status().as_u16(), 200);
+
+    let confirmed = sqlx::query!("SELECT status FROM subscriptions",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(confirmed.status, "confirmed");
 }
